f8abcde939e266390411f7e16bf9aeccf2411f6c
iksndwp <ikhsandwiputraa@gmail.com>
2026-02-08 09:16:30 +0700

    fix bind flask

--- app.py
+++ app.py
@@ -207,5 +207,8 @@ def dashboard():
         table=table_html
     )
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
